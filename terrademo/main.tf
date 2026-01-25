52a9e7ca349b5447a3e029725d626fd3dae1c7a0
mjguthier <28516868+mjguthier@users.noreply.github.com>
2026-01-25 22:55:47 +0000

    terraform demo

---
+++ terrademo/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "7.16.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "terraform-demo-485421"
+  region  = "us-west2"
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "terraform-demo-485421-terra-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
